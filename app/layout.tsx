d1cd389244f577ec36fcd96fb02040912a5f3520
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-02-28 19:27:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,45 @@
 import { ThirdwebProvider } from "thirdweb/react";
 import { Toaster } from "react-hot-toast";
 import { Navbar } from "@/components/Navbar";
-import Image from "next/image";
+import { SparklesCore } from "@/components/Sparkles";
 import "@/globals.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "BITNODES Marketplace",
-  description:
-		"monad Memes NFT marketplace.",
+  description: "monad Memes NFT marketplace.",
 };
 
 export default function RootLayout({
   children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="relative overflow-x-hidden max-w-screen">
-        <div className="absolute top-0 left-0 right-0 w-screen h-screen -z-10">
-          <Image
-            src="/hero-gradient.png"
-            width={1390}
-            height={1390}
-            alt="Background gradient from red to blue"
-            quality={100}
-            className="w-full h-full opacity-75"
+        {/* Ambient background with moving particles */}
+        <div className="h-full w-full absolute inset-0 z-0">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
           />
         </div>
 
-        <Toaster />
-        <ThirdwebProvider>
-          <Navbar />
-          <div className="w-screen min-h-screen">
-            <div className="px-8 mx-auto mt-32 max-w-7xl">
-              {children}
+        <div className="relative z-10">
+          <Toaster />
+          <ThirdwebProvider>
+            <Navbar />
+            <div className="w-screen min-h-screen">
+              <div className="px-8 mx-auto mt-32 max-w-7xl">{children}</div>
             </div>
-          </div>
-        </ThirdwebProvider>
+          </ThirdwebProvider>
+        </div>
       </body>
     </html>
   );
