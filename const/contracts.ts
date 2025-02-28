4c2898a634cc8527fb9b82f9128b65b5305b7f0b
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-02-28 15:35:44 +0200

    Update contracts.ts

--- const/contracts.ts
+++ const/contracts.ts
@@ -4,8 +4,29 @@ import client from "@/lib/client";
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { getContract } from "thirdweb";
-import { sepolia } from "thirdweb/chains";
-export const NETWORK = sepolia;
+//import { sepolia } from "thirdweb/chains";
+//export const NETWORK = sepolia;
+export const NETWORK = {
+        // === Required information for connecting to the network === \\
+        chainId: 10143, // Chain ID of the network
+        // Array of RPC URLs to use
+        rpc: ["https://testnet-rpc.monad.xyz"],
+
+        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+        // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+        nativeCurrency: {
+          decimals: 18,
+          name: "Monad Testnet",
+          symbol: "MON",
+        },
+        shortName: "Monad Testnet", // Display value shown in the wallet UI
+        slug: "Monad Testnet", // Display value shown in the wallet UI
+        testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "Monad Testnet", // Name of the network
+        name: "Monad Testnet", // Name of the network
+      }, {
+    secretKey: process.env.TW_SECRET_KEY,
+  };
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
