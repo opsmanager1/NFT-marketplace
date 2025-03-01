42f13887ff808447cb3739b467f91ee30c44002d
BITNODES <135651866+opsmanager1@users.noreply.github.com>
2025-03-01 22:11:29 +0200

    Update page.tsx

--- app/buy/page.tsx
+++ app/buy/page.tsx
@@ -7,8 +7,8 @@ import { MARKETPLACE, NFT_COLLECTION } from "@/const/contracts";
 
 export default function Buy() {
 	return (
-		<div className="">
-			<h1 className="text-4xl">Buy NFTs</h1>
+		<div className="container mx-auto px-4">
+			<h1 className="text-4xl font-bold text-center my-8">Buy NFTs</h1>
 
 			<div className="my-8">
 				<Suspense fallback={<NFTGridLoading />}>
